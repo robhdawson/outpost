4fd3ac83520967a23749aec6198c22cce0de9e17
Rob Dawson <robhdawson@gmail.com>
2017-05-20 16:43:27 -0400

    Empty out last of the create react app content

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -4,13 +4,13 @@ import './styles.scss';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
+      <div className="container">
+        <div className="header">
+
+          <div className="content">
+            <h1 className="title">Outpost.</h1>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
